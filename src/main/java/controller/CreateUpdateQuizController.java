3319d2770ded338d83a1a081d53f4673f1d4259c
Taco <taco.jongkind@gmail.com>
2020-10-28 17:06:21 +0100

    CreateUpdateQuiz buttons

--- src/main/java/controller/CreateUpdateQuizController.java
+++ src/main/java/controller/CreateUpdateQuizController.java
@@ -3,15 +3,26 @@ package controller;
 import database.mysql.DBAccess;
 import database.mysql.QuizDAO;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import model.Quiz;
 import view.Main;
 
 public class CreateUpdateQuizController {
+    public Button menuButton;
     private QuizDAO quizdao;
     private DBAccess dbAccess;
     private Quiz quiz;
+    public Button composeButton;
 
-    public void setup(Quiz quiz) {}
+    private Label titleLabel;
+
+    public CreateUpdateQuizController() {
+    }
+
+    public void setup(Quiz quiz) {
+        titlelable.set
+    }
 
     //TJ menu knop terug naar menu
     public void doMenu(ActionEvent actionEvent) {
