b046c62e4ec8da824119f39509711e233950f202
dleertouwer <daniel.leertouwer@hva.nl>
2020-10-29 22:43:11 +0100

    Added Mockito JUnit5 extension

--- src/test/controller/LoginControllerTest.java
+++ src/test/controller/LoginControllerTest.java
@@ -1,17 +1,73 @@
 package controller;
 
+import database.mysql.UserDAO;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 class LoginControllerTest {
 
-    @org.junit.jupiter.api.Test
+    @Mock
+    private UserDAO userDAO;
+    @Mock
+    private TextField nameTextField;
+    @Mock
+    private PasswordField passwordField;
+    @InjectMocks
+    private LoginController underTest;
+
+    @Test
     void doLogin() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void doQuit() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void showAlert() {
+    }
+
+    @Nested
+    class WhenDoingLogin {
+
+        @BeforeEach
+        void setup() {
+        }
+
+    }
+
+    @Nested
+    class WhenDoingQuit {
+
+        @Mock
+        private ActionEvent event;
+
+        @BeforeEach
+        void setup() {
+        }
+
+    }
+
+    @Nested
+    class WhenShowingAlert {
+
+        private final String MESSAGE = "MESSAGE";
+        private final AlertType TYPE = AlertType.NONE;
+
+        @BeforeEach
+        void setup() {
+        }
+
     }
 
 }
