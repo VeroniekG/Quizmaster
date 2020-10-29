f40eb90f066c75a5afb5496c0d29f9bd11771b1e
dleertouwer <daniel.leertouwer@hva.nl>
2020-10-29 20:37:57 +0100

    Added test

---
+++ src/test/controller/LoginControllerTest.java
@@ -0,0 +1,19 @@
+package controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginControllerTest {
+
+    @org.junit.jupiter.api.Test
+    void doLogin() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void doQuit() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void showAlert() {
+    }
+
+}
