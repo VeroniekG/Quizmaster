549009f6343ed9e3a3eb21e1d0e8ac70a18d8651
Taco <taco.jongkind@gmail.com>
2020-10-30 15:23:16 +0100

    quiz toegevoegd

--- src/main/java/model/Quiz.java
+++ src/main/java/model/Quiz.java
@@ -1,5 +1,37 @@
 package model;
 
-import javafx.scene.control.ListView;
+public class Quiz {
 
-public class Quiz extends ListView<Quiz> {}
+    private int idQuiz;
+    private String quizName;
+
+    public Quiz(int idQuiz, String quizName) {
+        this.idQuiz = idQuiz;
+        this.quizName = quizName;
+    }
+
+    public Quiz(String quizName) {
+        this.quizName = quizName;
+    }
+
+    @Override
+    public String toString() {
+        return "" + quizName + " ";
+    }
+
+    public int getIdQuiz() {
+        return idQuiz;
+    }
+
+    public void setIdQuiz(int idQuiz) {
+        this.idQuiz = idQuiz;
+    }
+
+    public String getQuizName() {
+        return quizName;
+    }
+
+    public void setQuizName(String quizName) {
+        this.quizName = quizName;
+    }
+}
