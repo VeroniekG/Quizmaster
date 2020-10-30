39c42cdae55d8162bef124f10a6ea01c51a52bbc
dleertouwer <daniel.leertouwer@hva.nl>
2020-10-30 02:47:37 +0100

    Mockito

--- src/test/controller/LoginControllerTest.java
+++ src/test/controller/LoginControllerTest.java
@@ -8,12 +8,9 @@ import javafx.scene.control.TextField;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 
-@ExtendWith(MockitoExtension.class)
 class LoginControllerTest {
 
     @Mock
