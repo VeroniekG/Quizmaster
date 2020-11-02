da9ada361ef69fbe5c4d0acf62653a2b425b2910
Taco <taco.jongkind@gmail.com>
2020-11-02 14:32:15 +0100

    Tests en aanpassing CreateUpdateQuizController

---
+++ src/test/controller/CreateUpdateQuizControllerTest.java
@@ -0,0 +1,28 @@
+package controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CreateUpdateQuizControllerTest {
+
+    @Test
+    void setup() {
+    }
+
+    @Test
+    void doMenu() {
+    }
+
+    @Test
+    void createQuiz() {
+    }
+
+    @Test
+    void doStoreQuiz() {
+    }
+
+    @Test
+    void doCreateUpdateQuiz() {
+    }
+}
---
+++ src/test/model/QuizTest.java
@@ -0,0 +1,28 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuizTest {
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void getIdQuiz() {
+    }
+
+    @Test
+    void setIdQuiz() {
+    }
+
+    @Test
+    void getQuizName() {
+    }
+
+    @Test
+    void setQuizName() {
+    }
+}
