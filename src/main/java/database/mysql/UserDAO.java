368e6d80aed9b5b69420ff2c62a53fba84d75542
dleertouwer <daniel.leertouwer@hva.nl>
2020-11-03 10:33:18 +0100

    Refactored UserDAO

--- src/main/java/database/mysql/UserDAO.java
+++ src/main/java/database/mysql/UserDAO.java
@@ -1,7 +1,6 @@
 package database.mysql;
 
 import controller.CreateUpdateUserController;
-import model.Question;
 import model.Role;
 import model.User;
 import org.apache.logging.log4j.LogManager;
@@ -11,25 +10,56 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Interacts with the User model and maps application calls to the persistence layer. Extends
  * {@link AbstractDAO} and contains methods to retrieve all users, to retrieve one user by name, to
  * retrieve one user by id and to store one user.
- * TODO: add updateOne() to GenericDAO
  *
- * @author dleertouwer
- * @version 1.0.9
+ * @author Daniel Leertouwer
+ * @version 1.0.15
  * @see User
- * @since 1.0.0
+ * @since 1.0
  */
-public class UserDAO extends AbstractDAO implements GenericDAO<User> {
+public class UserDAO extends AbstractDAO implements GenericDAO<User>, DAO<User> {
 
     private static final Logger LOGGER = LogManager.getLogger(UserDAO.class);
 
     public UserDAO(DBAccess dBaccess) {
         super(dBaccess);
+    }
+
+    /**
+     * Retrieves a user from the database by username. A PreparedStatement is used to execute a
+     * parameterized SQL-query with BINARY function. This function converts userName to a binary
+     * string, which makes the query case-sensitive. {@link #setUserWithResultSet(ResultSet)} is
+     * called with the resulting ResultSet to convert the contained values to a corresponding
+     * User object.
+     *
+     * @param name the username as a String
+     *
+     * @return an object of type User
+     * @throws SQLException if the Parameter index is out of range; if a database access error
+     * occurs; if other SQL-errors occur
+     * @see #setupPreparedStatement(String sql)
+     * @see ResultSet
+     * @since 1.0
+     */
+    @Override
+    public User getOneByName(String name) {
+        String sql = "SELECT * FROM User WHERE BINARY userName = ?";
+        User user = null;
+        try {
+            setupPreparedStatement(sql);
+            preparedStatement.setString(1, name);
+            ResultSet resultSet = executeSelectStatement();
+            if (resultSet.next()) {
+                user = setUserWithResultSet(resultSet);
+            }
+        } catch (SQLException sqlException) {
+            LOGGER.error("SQL-error " + sqlException.getMessage());
+        }
+        return user;
     }
 
     /**
@@ -42,9 +72,9 @@ public class UserDAO extends AbstractDAO implements GenericDAO<User> {
      * @throws SQLException if one or more columnLabels are not valid; if a database access
      * error occurs; if this method is called with a closed ResultSet
      * @see ResultSet
-     * @since 1.0.0
+     * @since 1.0
      */
-    private User setUserWithResultset(ResultSet resultSet) throws SQLException {
+    private User setUserWithResultSet(ResultSet resultSet) throws SQLException {
         int idUser = resultSet.getInt("idUser");
         String userName = resultSet.getString("userName");
         String password = resultSet.getString("password");
@@ -54,43 +84,76 @@ public class UserDAO extends AbstractDAO implements GenericDAO<User> {
         return new User(idUser, firstName, lastName, userName, password, Role.valueOf(role));
     }
 
-    /**
-     * Retrieves a user from the database by username. A PreparedStatement is used to execute a
-     * parameterized SQL-query with BINARY function. This function converts userName to a binary
-     * string, which makes the query case-sensitive. {@link #setUserWithResultset(ResultSet)} is
-     * called with the resulting ResultSet to convert the contained values to a corresponding
-     * User object.
-     *
-     * @param name the username as a String
-     *
-     * @return an object of type User
-     * @throws SQLException if the Parameter index is out of ronge; if a database access error
-     * occurs; if other SQL-errors occur
-     * @see #setupPreparedStatement(String sql)
-     * @see ResultSet
-     * @since 1.0.0
-     */
-    public User getOneByName(String name) {
-        String sql = "SELECT * FROM User WHERE BINARY userName = ?";
-        User user = null;
+    public void updateOne(User type) {
+        String sql = "UPDATE User SET userName = ?, password = ?, role = ?, firstName = ?, " +
+                "lastName = ? where idUser = ?;";
         try {
             setupPreparedStatement(sql);
-            preparedStatement.setString(1, name);
+            setPreparedStatementParameters(type);
+            executeManipulateStatement();
+        } catch (SQLIntegrityConstraintViolationException duplicateUserName) {
+            LOGGER.error("Gebruikersnaam bestaat al!");
+            CreateUpdateUserController.setSqlErrorMessage("Gebruikersnaam bestaat al!");
+        } catch (SQLException sqlException) {
+            LOGGER.error("SQL-error: " + sqlException.getMessage());
+        }
+    }
+
+    private void setPreparedStatementParameters(User user) throws SQLException {
+        preparedStatement.setString(1, user.getUserName());
+        preparedStatement.setString(2, user.getPassword());
+        preparedStatement.setString(3, user.getRole().name());
+        preparedStatement.setString(4, user.getFirstName());
+        preparedStatement.setString(5, user.getLastName());
+        // If the user isn't a new user (idUser == 0), we also need idUser to be able to execute an
+        // UPDATE statement
+        if (user.getIdUser() != 0) {
+            preparedStatement.setInt(6, user.getIdUser());
+        }
+    }
+
+    public void deleteOne(User type) {
+        String sql = "DELETE FROM User WHERE idUser = ?;";
+        try {
+            setupPreparedStatement(sql);
+            preparedStatement.setInt(1, type.getIdUser());
+            executeManipulateStatement();
+        } catch (SQLException sqlException) {
+            LOGGER.error("SQL-error: " + sqlException.getMessage());
+        }
+    }
+
+    /**
+     * Retrieves all users from the database. A PreparedStatement is used to execute a
+     * parameterized SQL-query. {@link #setUserWithResultSet(ResultSet)} is called with the
+     * resulting ResultSet to convert the contained values to a corresponding User object
+     *
+     * @return an ArrayList with objects of type User
+     * @throws SQLException if one or more columnLabels are not valid; if a database access
+     * error occurs; if other SQL-errors occur
+     * @since 1.0
+     */
+    @Override
+    public ArrayList<User> getAll() {
+        String sql = "SELECT * FROM User";
+        ArrayList<User> userList = new ArrayList<>();
+        try {
+            setupPreparedStatement(sql);
             ResultSet resultSet = executeSelectStatement();
-            if (resultSet.next()) {
-                user = setUserWithResultset(resultSet);
-            } else {
-                LOGGER.debug("User '" + name + "' not found!");
+            User user;
+            while (resultSet.next()) {
+                user = setUserWithResultSet(resultSet);
+                userList.add(user);
             }
         } catch (SQLException sqlException) {
             LOGGER.error("SQL-error " + sqlException.getMessage());
         }
-        return user;
+        return userList;
     }
 
     /**
      * Retrieves a user from the database by idUser. A PreparedStatement is used to execute a
-     * parameterized SQL-query. {@link #setUserWithResultset(ResultSet)} is called with the
+     * parameterized SQL-query. {@link #setUserWithResultSet(ResultSet)} is called with the
      * resulting ResultSet to convert the contained values to a corresponding User object.
      *
      * @return an object of type User
@@ -99,7 +162,7 @@ public class UserDAO extends AbstractDAO implements GenericDAO<User> {
      * occurs; if other SQL-error occur
      * @see #setupPreparedStatement(String sql)
      * @see ResultSet
-     * @since 1.0.0
+     * @since 1.0
      */
     @Override
     public User getOneById(int id) {
@@ -110,7 +173,7 @@ public class UserDAO extends AbstractDAO implements GenericDAO<User> {
             preparedStatement.setInt(1, id);
             ResultSet resultSet = executeSelectStatement();
             if (resultSet.next()) {
-                user = setUserWithResultset(resultSet);
+                user = setUserWithResultSet(resultSet);
             } else {
                 throw new IllegalArgumentException("User with id '" + id + "' not found!");
             }
@@ -131,7 +194,7 @@ public class UserDAO extends AbstractDAO implements GenericDAO<User> {
      * occurs; if other SQL-error occur
      * @see #setupPreparedStatementWithKey(String sql)
      * @see #executeInsertStatementWithKey()
-     * @since 1.0.0
+     * @since 1.0
      */
     @Override
     public void storeOne(User type) {
@@ -149,71 +212,6 @@ public class UserDAO extends AbstractDAO implements GenericDAO<User> {
         }
     }
 
-    public void updateOne(User type) {
-        String sql = "UPDATE User SET userName = ?, password = ?, role = ?, firstName = ?, " +
-                "lastName = ? where idUser = ?;";
-        try {
-            setupPreparedStatement(sql);
-            setPreparedStatementParameters(type);
-            executeManipulateStatement();
-        } catch (SQLIntegrityConstraintViolationException duplicateUserName) {
-            LOGGER.error("Gebruikersnaam bestaat al!");
-            CreateUpdateUserController.setSqlErrorMessage("Gebruikersnaam bestaat al!");
-        } catch (SQLException sqlException) {
-            LOGGER.error("SQL-error: " + sqlException.getMessage());
-        }
-    }
-
-    public void deleteOne(User type) {
-        String sql = "DELETE FROM User WHERE idUser = ?;";
-        try {
-            setupPreparedStatement(sql);
-            preparedStatement.setInt(1, type.getIdUser());
-            executeManipulateStatement();
-        } catch (SQLException sqlException) {
-            LOGGER.error("SQL-error: " + sqlException.getMessage());
-        }
-    }
-
-    /**
-     * Retrieves all users from the database. A PreparedStatement is used to execute a
-     * parameterized SQL-query. {@link #setUserWithResultset(ResultSet)} is called with the
-     * resulting ResultSet to convert the contained values to a corresponding User object
-     *
-     * @return an ArrayList with objects of type User
-     * @throws SQLException if one or more columnLabels are not valid; if a database access
-     * error occurs; if other SQL-errors occur
-     */
-    @Override
-    public ArrayList<User> getAll() {
-        String sql = "SELECT * FROM User";
-        ArrayList<User> userslist = new ArrayList<>();
-        try {
-            setupPreparedStatement(sql);
-            ResultSet resultSet = executeSelectStatement();
-            User user;
-            while (resultSet.next()) {
-                user = setUserWithResultset(resultSet);
-                userslist.add(user);
-            }
-        } catch (SQLException sqlException) {
-            LOGGER.error("SQL-error " + sqlException.getMessage());
-        }
-        return userslist;
-    }
-
-    private void setPreparedStatementParameters(User user) throws SQLException {
-        preparedStatement.setString(1, user.getUserName());
-        preparedStatement.setString(2, user.getPassword());
-        preparedStatement.setString(3, user.getRole().name());
-        preparedStatement.setString(4, user.getFirstName());
-        preparedStatement.setString(5, user.getLastName());
-        // If the user isn't a new user (idUser == 0), we also need idUser to be able to execute an
-        // UPDATE statement
-        if (user.getIdUser() != 0) {
-            preparedStatement.setInt(6, user.getIdUser());
-        }
-    }
     //@VG - retrieve users from DB per specific rol
     public ArrayList<User> getUserByRole() {
         String sql = "SELECT * FROM user WHERE role ='COORDINATOR'";
@@ -234,4 +232,5 @@ public class UserDAO extends AbstractDAO implements GenericDAO<User> {
         }
         return rolelist;
     }
+
 }
