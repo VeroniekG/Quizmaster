34bd16fad0116287d50afde0a48bdec2dd3caf31
dleertouwer <daniel.leertouwer@hva.nl>
2020-11-03 11:12:31 +0100

    Minor changes CreateUpdateUserController, ManageUsersController

--- src/main/java/controller/ManageUsersController.java
+++ src/main/java/controller/ManageUsersController.java
@@ -3,16 +3,12 @@ package controller;
 import database.mysql.UserDAO;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ListView;
-import javafx.scene.input.InputEvent;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.input.TouchEvent;
 import model.Role;
+import model.Session;
 import model.User;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -22,9 +18,15 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * TODO: Refactor to make use of services
+ *
+ * @author Daniel Leertouwer
+ */
 public class ManageUsersController {
 
     private static final Logger LOGGER = LogManager.getLogger(ManageUsersController.class);
+    private static final Session session = Session.getInstance();
     public static User selectedUser;
     @FXML
     private ListView<User> userList;
@@ -37,49 +39,12 @@ public class ManageUsersController {
 
     public void setup() {
         populateList();
-        if (selectedUser == null) {
+        if (session.getSelectedUser() == null) {
             selectFirstListItem();
         } else {
-            userList.getSelectionModel().select(selectedUser);
+            userList.getSelectionModel().select(session.getSelectedUser());
         }
-        addEventHandlers();
-    }
-
-    public void doMenu() {
-        Main.getSceneManager().showWelcomeScene();
-    }
-
-    public void doCreateUser() {
-        selectedUser = new User("", "", "", "", Role.STUDENT);
-        Main.getSceneManager().showCreateUpdateUserScene(selectedUser);
-    }
-
-    public void doUpdateUser() {
-        updateSelectedUser();
-        if (isNotCurrentUser()) {
-            Main.getSceneManager().showCreateUpdateUserScene(selectedUser);
-        } else {
-            showAlertCannotUpdate();
-        }
-    }
-
-    public void doDeleteUser() {
-        updateSelectedUser();
-        if (isNotCurrentUser()) {
-            Optional<ButtonType> buttonType = showAlertDelete(selectedUser);
-            ButtonType buttonPressed = buttonType.orElse(ButtonType.CANCEL);
-            if (buttonPressed == ButtonType.OK) {
-                userDAO.deleteOne(selectedUser);
-                populateList();
-                selectFirstListItem();
-            } else {
-                showAlertCancelled();
-                updateSelectedUser();
-            }
-        } else {
-            showAlertCannotUpdate();
-        }
-
+        //        addEventHandlers();
     }
 
     public void populateList() {
@@ -95,17 +60,76 @@ public class ManageUsersController {
         updateSelectedUser();
     }
 
+    //    public void addEventHandlers() {
+    //        EventHandler<InputEvent> selectionHandler = inputEvent -> {
+    //            updateSelectedUser();
+    //        };
+    //        userList.addEventHandler(MouseEvent.MOUSE_CLICKED, selectionHandler);
+    //        userList.addEventHandler(TouchEvent.TOUCH_PRESSED, selectionHandler);
+    //        userList.addEventHandler(KeyEvent.KEY_PRESSED, selectionHandler);
+    //    }
+
     public void updateSelectedUser() {
-        selectedUser = userList.getSelectionModel().getSelectedItem();
+        //        selectedUser = userList.getSelectionModel().getSelectedItem();
+        session.setSelectedUser(userList.getSelectionModel().getSelectedItem());
     }
 
-    public void addEventHandlers() {
-        EventHandler<InputEvent> selectionHandler = inputEvent -> {
-            updateSelectedUser();
-        };
-        userList.addEventHandler(MouseEvent.MOUSE_CLICKED, selectionHandler);
-        userList.addEventHandler(TouchEvent.TOUCH_PRESSED, selectionHandler);
-        userList.addEventHandler(KeyEvent.KEY_PRESSED, selectionHandler);
+    public void doMenu() {
+        Main.getSceneManager().showWelcomeScene();
+    }
+
+    public void doCreateUser() {
+        //        selectedUser = new User("", "", "", "", Role.STUDENT);
+        //        Main.getSceneManager().showCreateUpdateUserScene(selectedUser);
+        User user = new User("", "", "", "", Role.STUDENT);
+        Main.getSceneManager().showCreateUpdateUserScene(user);
+    }
+
+    public void doUpdateUser() {
+        updateSelectedUser();
+        if (isNotCurrentUser()) {
+            Main.getSceneManager().showCreateUpdateUserScene(session.getSelectedUser());
+        } else {
+            showAlertCannotUpdate();
+        }
+    }
+
+    public boolean isNotCurrentUser() {
+        boolean isNotCurrentUser = true;
+        //User currentUser = Main.getCurrentUser();
+        User currentUser = session.getLoggedInUser();
+        if (currentUser.equals(session.getSelectedUser())) {
+            isNotCurrentUser = false;
+        }
+        return isNotCurrentUser;
+    }
+
+    private void showAlertCannotUpdate() {
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("Meldingvenster");
+        alert.setHeaderText("Waarschuwing");
+        alert.setContentText("Je bent ingelogd als '" + session.getLoggedInUser().getUserName() + "'. " +
+                "Gebruiker kan niet worden bewerkt of verwijderd!");
+        alert.show();
+    }
+
+    public void doDeleteUser() {
+        updateSelectedUser();
+        if (isNotCurrentUser()) {
+            Optional<ButtonType> buttonType = showAlertDelete(session.getSelectedUser());
+            ButtonType buttonPressed = buttonType.orElse(ButtonType.CANCEL);
+            if (buttonPressed == ButtonType.OK) {
+                userDAO.deleteOne(session.getSelectedUser());
+                populateList();
+                selectFirstListItem();
+            } else {
+                showAlertCancelled();
+                updateSelectedUser();
+            }
+        } else {
+            showAlertCannotUpdate();
+        }
+
     }
 
     private Optional<ButtonType> showAlertDelete(User user) {
@@ -123,27 +147,9 @@ public class ManageUsersController {
         alert.show();
     }
 
-    private void showAlertCannotUpdate() {
-        Alert alert = new Alert(Alert.AlertType.WARNING);
-        alert.setTitle("Meldingvenster");
-        alert.setHeaderText("Waarschuwing");
-        alert.setContentText("Je bent ingelogd als '" + selectedUser.getUserName() + "'. " +
-                "Gebruiker kan niet worden bewerkt of verwijderd!");
-        alert.show();
-    }
-
-    public boolean isNotCurrentUser() {
-        boolean isNotCurrentUser = true;
-        User currentUser = Main.getCurrentUser();
-        if (currentUser.equals(selectedUser)) {
-            isNotCurrentUser = false;
-        }
-        return isNotCurrentUser;
-    }
-
-    public static void setSelectedUser(User selectedUser) {
-        ManageUsersController.selectedUser = selectedUser;
-    }
+    //    public static void setSelectedUser(User selectedUser) {
+    //        ManageUsersController.selectedUser = selectedUser;
+    //    }
 
     private enum ButtonOperation {
         OK, CANCEL, NONE;
