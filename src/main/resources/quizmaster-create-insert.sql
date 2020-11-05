523ef72a5bd68a93df01971d918f67f87efb9dda
dleertouwer <daniel.leertouwer@hva.nl>
2020-11-05 18:51:10 +0100

    Fixed create-insert script

--- src/main/resources/quizmaster-create-insert.sql
+++ src/main/resources/quizmaster-create-insert.sql
@@ -8,6 +8,17 @@ SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE =
         'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
+-- Schema Quizmaster
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `Quizmaster`;
+
+-- -----------------------------------------------------
+-- Schema Quizmaster
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `Quizmaster` DEFAULT CHARACTER SET utf8;
+USE `Quizmaster`;
+
+-- -----------------------------------------------------
 -- Database user for Schema Quizmaster
 -- -----------------------------------------------------
 CREATE USER IF NOT EXISTS 'userQuizmaster'@'localhost'
@@ -27,7 +38,14 @@ CREATE TABLE IF NOT EXISTS `Quizmaster`.`Course`
 (
     `idCourse`   INT         NOT NULL AUTO_INCREMENT,
     `courseName` VARCHAR(45) NOT NULL,
-    PRIMARY KEY (`idCourse`)
+    `idCoordinator` INT NULL,
+    PRIMARY KEY (`idCourse`),
+    INDEX `idCoordinator_idx` (`idCoordinator` ASC) VISIBLE,
+    CONSTRAINT `fk_idCoordinator`
+        FOREIGN KEY (`idCoordinator`)
+            REFERENCES `quizmaster`.`user` (`idUser`)
+            ON DELETE SET NULL
+            ON UPDATE CASCADE
 )
     ENGINE = InnoDB
     AUTO_INCREMENT = 13
@@ -39,30 +57,30 @@ CREATE TABLE IF NOT EXISTS `Quizmaster`.`Course`
 START TRANSACTION;
 USE `Quizmaster`;
 
-INSERT INTO Course (idCourse, courseName)
-VALUES (1, 'SkillUp');
-INSERT INTO Course (idCourse, courseName)
-VALUES (2, 'No-Bull Bootcamp');
-INSERT INTO Course (idCourse, courseName)
-VALUES (3, 'Mentee to Mentor');
-INSERT INTO Course (idCourse, courseName)
-VALUES (4, 'Active Achievement');
-INSERT INTO Course (idCourse, courseName)
-VALUES (5, 'Practice to Perfect');
-INSERT INTO Course (idCourse, courseName)
-VALUES (6, 'Strive Training');
-INSERT INTO Course (idCourse, courseName)
-VALUES (7, 'Commission Kings');
-INSERT INTO Course (idCourse, courseName)
-VALUES (8, 'Productivity Today');
-INSERT INTO Course (idCourse, courseName)
-VALUES (9, 'Unbound Opportunities');
-INSERT INTO Course (idCourse, courseName)
-VALUES (10, 'Passion Chasers');
-INSERT INTO Course (idCourse, courseName)
-VALUES (11, 'Limitless Horizons');
-INSERT INTO Course (idCourse, courseName)
-VALUES (12, 'Excalibur Training');
+INSERT INTO Course (idCourse, courseName, idCoordinator)
+VALUES (1, 'SkillUp', 3);
+INSERT INTO Course (idCourse, courseName, idCoordinator)
+VALUES (2, 'No-Bull Bootcamp', 3);
+INSERT INTO Course (idCourse, courseName, idCoordinator)
+VALUES (3, 'Mentee to Mentor', 3);
+INSERT INTO Course (idCourse, courseName, idCoordinator)
+VALUES (4, 'Active Achievement', 3);
+INSERT INTO Course (idCourse, courseName, idCoordinator)
+VALUES (5, 'Practice to Perfect', 3);
+INSERT INTO Course (idCourse, courseName, idCoordinator)
+VALUES (6, 'Strive Training', 3);
+INSERT INTO Course (idCourse, courseName, idCoordinator)
+VALUES (7, 'Commission Kings', 3);
+INSERT INTO Course (idCourse, courseName, idCoordinator)
+VALUES (8, 'Productivity Today', 3);
+INSERT INTO Course (idCourse, courseName, idCoordinator)
+VALUES (9, 'Unbound Opportunities', 3);
+INSERT INTO Course (idCourse, courseName, idCoordinator)
+VALUES (10, 'Passion Chasers', 3);
+INSERT INTO Course (idCourse, courseName, idCoordinator)
+VALUES (11, 'Limitless Horizons', 3);
+INSERT INTO Course (idCourse, courseName, idCoordinator)
+VALUES (12, 'Excalibur Training', 3);
 
 COMMIT;
 
@@ -75,14 +93,19 @@ CREATE TABLE IF NOT EXISTS `Quizmaster`.`Course_User`
 (
     `idUser`   INT NOT NULL,
     `idCourse` INT NOT NULL,
+    PRIMARY KEY (`idUser`, `idCourse`),
     INDEX `fk_Course_User` (`idUser` ASC) VISIBLE,
     INDEX `fk_User_Course` (`idCourse` ASC) VISIBLE,
     CONSTRAINT `fk_Course_User`
         FOREIGN KEY (`idUser`)
-            REFERENCES `quizmaster`.`user` (`idUser`),
+            REFERENCES `quizmaster`.`user` (`idUser`)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE,
     CONSTRAINT `fk_User_Course`
         FOREIGN KEY (`idCourse`)
             REFERENCES `quizmaster`.`course` (`idCourse`)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE
 )
     ENGINE = InnoDB
     DEFAULT CHARACTER SET = utf8;
@@ -101,13 +124,13 @@ CREATE TABLE IF NOT EXISTS `Quizmaster`.`Question`
     answerWrong2 VARCHAR(45)  NOT NULL,
     answerWrong3 VARCHAR(45)  NOT NULL,
     idQuiz       INT          NOT NULL,
-    PRIMARY KEY (idQuestion, idQuiz),
+    PRIMARY KEY (idQuestion),
     INDEX verzinzelf1_idx (idQuiz ASC) VISIBLE,
     CONSTRAINT verzinzelf1
         FOREIGN KEY (idQuiz)
             REFERENCES Quizmaster.Quiz (idQuiz)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION
+            ON DELETE CASCADE
+            ON UPDATE CASCADE
 )
     ENGINE = InnoDB
     AUTO_INCREMENT = 8
@@ -145,20 +168,22 @@ VALUES (11, 'Wat is 5+2?', '7', '54545', '755', '327', 11);
 COMMIT;
 
 -- -----------------------------------------------------
--- Table `Quizmaster`.`Quiz` ---------------------- TODO
+-- Table `Quizmaster`.`Quiz` ---------------------------
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `Quizmaster`.`Quiz`;
 
 CREATE TABLE IF NOT EXISTS `Quizmaster`.`Quiz`
 (
     `idQuiz`   INT         NOT NULL AUTO_INCREMENT,
-    `nameQuiz` VARCHAR(25) NOT NULL,
-    `idCursus` INT         NOT NULL,
+    `quizName` VARCHAR(25) NOT NULL,
+    `idCourse` INT         NOT NULL,
     PRIMARY KEY (`idQuiz`),
-    INDEX `fk_Quizzes_Course1_idx` (`idCursus` ASC) VISIBLE,
+    INDEX `fk_Quizzes_Course1_idx` (`idCourse` ASC) VISIBLE,
     CONSTRAINT `fk_Quizzes_Course1`
-        FOREIGN KEY (`idCursus`)
+        FOREIGN KEY (`idCourse`)
             REFERENCES `Quizmaster`.`Course` (`idCourse`)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE
 )
     ENGINE = InnoDB
     AUTO_INCREMENT = 1
@@ -184,7 +209,7 @@ INSERT INTO `quizmaster`.`quiz` (`idQuiz`, `idCourse`, `quizName`) VALUES (14, 7
 INSERT INTO `quizmaster`.`quiz` (`idQuiz`, `idCourse`, `quizName`) VALUES (15, 7, 'Top Pop');
 
 -- -----------------------------------------------------
--- Table `Quizmaster`.`Quizresult` ---------------- TODO
+-- Table `Quizmaster`.`Quizresult` ---------------------
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `Quizmaster`.`QuizResult`;
 
@@ -201,10 +226,14 @@ CREATE TABLE IF NOT EXISTS `Quizmaster`.`QuizResult`
     INDEX `fk_QuizResult_Quizzes1_idx` (`Quizzes_idQuizzes` ASC) VISIBLE,
     CONSTRAINT `fk_QuizResult_Quizzes1`
         FOREIGN KEY (`Quizzes_idQuizzes`)
-            REFERENCES `Quizmaster`.`Quiz` (`idQuiz`),
+            REFERENCES `Quizmaster`.`Quiz` (`idQuiz`)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE,
     CONSTRAINT `fk_QuizResult_User1`
         FOREIGN KEY (`User_idUser`)
             REFERENCES `Quizmaster`.`User` (`idUser`)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE
 )
     ENGINE = InnoDB
     AUTO_INCREMENT = 1
@@ -230,19 +259,27 @@ CREATE TABLE IF NOT EXISTS `Quizmaster`.`StudentGroup`
     CONSTRAINT `fk_Group_Course`
         FOREIGN KEY (`idCourse`)
             REFERENCES `Quizmaster`.`Course` (`idCourse`)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE
 )
     ENGINE = InnoDB
     AUTO_INCREMENT = 2
     DEFAULT CHARACTER SET = utf8;
 
 -- -----------------------------------------------------
--- Data for table `Quizmaster`.`StudentGroup` ----- TODO
+-- Data for table `Quizmaster`.`StudentGroup` ----------
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `Quizmaster`;
 
 INSERT INTO `Quizmaster`.`StudentGroup` (`idStudentGroup`, `StudentGroupName`, `idCourse`)
-VALUES (1, 'Group1', 1);
+VALUES (1, 'Groep 1', 1);
+INSERT INTO `Quizmaster`.`StudentGroup` (`idStudentGroup`, `StudentGroupName`, `idCourse`)
+VALUES (2, 'Groep 2', 1);
+INSERT INTO `Quizmaster`.`StudentGroup` (`idStudentGroup`, `StudentGroupName`, `idCourse`)
+VALUES (3, 'Groep 3', 1);
+INSERT INTO `Quizmaster`.`StudentGroup` (`idStudentGroup`, `StudentGroupName`, `idCourse`)
+VALUES (4, 'Groep 4', 1);
 
 COMMIT;
 
@@ -291,7 +328,6 @@ INSERT INTO `Quizmaster`.`User` (`idUser`, `userName`, `password`, `role`, `firs
 VALUES ('9', 'Student3', 'pw3', 'STUDENT', 'Student3', 'Drie');
 INSERT INTO `Quizmaster`.`User` (`idUser`, `userName`, `password`, `role`, `firstName`, `lastName`)
 VALUES ('10', 'Student4', 'pw4', 'STUDENT', 'Student4', 'Vier');
-
 
 COMMIT;
 
